88fed151f522ccbbced0fccccdc4097de10353f9
jimmytrevor <jimmytrevorsseg077@gmail.com>
2021-10-11 12:46:17 +0300

    App Changes

--- app/src/main/java/com/example/resultapp/HomeScreen.java
+++ app/src/main/java/com/example/resultapp/HomeScreen.java
@@ -2,10 +2,13 @@ package com.example.resultapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -31,5 +34,31 @@ public class HomeScreen extends AppCompatActivity {
     }
     public void add_new_results(View view) {
         startActivity(new Intent(HomeScreen.this,AddMarksActivity.class));
+    }
+
+    public void logout(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(HomeScreen.this)
+                .setTitle("Logout Confirmation")
+                .setMessage("Are you sure you want to logout")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                       SharedPreferences.Editor editor= sharedPreferences.edit();
+                       editor.clear();
+                       editor.apply();
+                       editor.commit();
+                       startActivity( new Intent(HomeScreen.this, LoginActivity.class));
+                       finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+        AlertDialog dialog=builder.create();
+        dialog.show();
     }
 }
